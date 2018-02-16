ef0e2a0d57399e6cb58d3811ce9efc992d81e170
Scott Faber <scottfaberphoto@gmail.com>
2018-02-16 16:52:32 -0500

    took buttons buy buttons off packages page, replaced with link to scheduleing page

--- front/src/components/packages.js
+++ front/src/components/packages.js
@@ -1,4 +1,5 @@
 import React, {Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class Packages extends Component {
 render() {
@@ -10,39 +11,21 @@ render() {
         <h3>Squeaky Clean </h3>
   			<p>Exterior only wash, clear coat shine, clear coat protectant, and underbody wash.  </p>
   			<p> $25 </p>
-        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-          <input type="hidden" name="cmd" value="_s-xclick"/>
-          <input type="hidden" name="hosted_button_id" value="SED4SVYCZTFZ6"/>
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
-          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
-        </form>
-
+            <Link to='/schedule'> Schedule Your Squeaky Clean Wash! </Link>
         </div>
 
         <div className="box">
         <h3>Squeaky Deluxe </h3>
         <p> Full service wash, clear coat shine, clear coat protectant, underbody wash, tire shine and rim cleaning. </p>
         <p> $50 </p>
-        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-          <input type="hidden" name="cmd" value="_s-xclick"/>
-          <input type="hidden" name="hosted_button_id" value="AJDH78UK9SSAA"/>
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
-          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
-        </form>
-
+            <Link to='/schedule'> Schedule Your Squeaky Clean Wash! </Link>
         </div>
 
         <div className="box">
         <h3>Squeaky Ultimate </h3>
         <p>Full service wash, mat shampoo, clear coat protectant,  clear coat shine, coat protectant, underbody wash, tire shine, rim cleaning, polish wax, interior fragrance  .</p>
         <p>$100</p>
-        <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-          <input type="hidden" name="cmd" value="_s-xclick"/>
-          <input type="hidden" name="hosted_button_id" value="M4X8REGCZ8S22"/>
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
-          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
-        </form>
-
+            <Link to='/schedule'> Schedule Your Squeaky Clean Wash! </Link>
         </div>		
 
       </div>
