9a9d90e0d8acaa998c8e816a78e6310e533e14fb
nnm0122 <nnm0122@gmail.com>
2018-02-17 02:48:15 -0500

    added text about us on home page

--- front/src/components/home.js
+++ front/src/components/home.js
@@ -1,11 +1,21 @@
 import React, {Component } from 'react';
-
+import {Link} from 'react-router-dom';
 class Home extends Component {
 render() {
   return (
-      <div className="home">
+      <div className="home page">
+           <video id="background-video" loop autoPlay>
++		    <source src={require("../static/img/background-movie3.mp4")} type="video/mp4" />
++		    Your browser does not support the video tag.
++		</video>
+ 
+       </div>
+     );
 
-      Home
+     <h1>About Us</h1>
+     <p>Squeaky clean is a mobile car wash service. Our mobile team will come to you to provide our outstanding car detailing service.  Booking is simple. All we need is a location and your keys!</p>
+
+      <p> Check out our <Link to="/packages"> packages</Link> to learn more about our services.</p>
 
       </div>
     );
